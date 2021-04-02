a24536252b7134b65007e68297ff782181bc157e
Nohet <70505658+Nohet@users.noreply.github.com>
2021-04-02 09:49:31 +0200

    Update moderation.py

--- Cogs/moderation.py
+++ Cogs/moderation.py
@@ -1,15 +1,38 @@
 import discord
 from discord.ext import commands
 from database import *
+import asyncio
 
+def convert(time):
+    pos = ["s","m","h","d"]
+
+    time_dict = {"s" : 1, "m" : 60, "h" : 3600 , "d" : 3600*24}
+
+    unit = time[-1]
+
+    if unit not in pos:
+        return -1
+    try:
+        val = int(time[:-1])
+    except:
+        return -2
+
+
+    return val * time_dict[unit]
 
 class ModerationCog(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
+
     @commands.Cog.listener()
     async def on_ready(self):
         print("Successfully loaded moderation.py")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send(error)
 
     @commands.command()
     @commands.guild_only()
@@ -212,6 +235,55 @@ class ModerationCog(commands.Cog):
             )
             embed.add_field(name="Sukces", value=f"Pomyślnie ustawiono slowmode na {time}s")
             await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.has_permissions(kick_members=True)
+    async def tempmute(self, ctx, member: discord.Member, time, *, reason):
+        reqlanguage = guildsett.find_one({"_id": ctx.message.guild.id})
+        language = reqlanguage["language"]
+        getmuterole = guildsett.find_one({"_id": ctx.message.guild.id})
+        rolemute = getmuterole["muterole"]
+        sleeptime = convert(time)
+        if language == ("en"):
+            embed = discord.Embed(
+                colour=discord.Color.from_rgb(244, 182, 89)
+            )
+            embed.set_author(name="TempMute")
+            embed.add_field(name="Nick:", value=f"{member}")
+            embed.add_field(name="Admin:", value=ctx.message.author)
+            embed.add_field(name="Reason:", value=reason)
+            embed.add_field(name="Time:", value=time)
+            embed.set_image(url="https://media.giphy.com/media/My6LerZy9kQLwObqWk/giphy.gif")
+            await ctx.send(embed=embed)
+        elif language == ("pl"):
+            embed = discord.Embed(
+                colour=discord.Color.from_rgb(244, 182, 89)
+            )
+            embed.set_author(name="TempMute")
+            embed.add_field(name="Nazwa:", value=f"{member}")
+            embed.add_field(name="Moderator:", value=ctx.message.author)
+            embed.add_field(name="Powód:", value=reason)
+            embed.add_field(name="Czas:", value=time)
+            embed.set_image(url="https://media.giphy.com/media/My6LerZy9kQLwObqWk/giphy.gif")
+            await ctx.send(embed=embed)
+        guild = ctx.guild
+        muteRole = discord.utils.get(guild.roles, name=rolemute)
+
+        if not muteRole:
+            muteRole = await guild.create_role(name=rolemute)
+
+            for channel in guild.channels:
+                await channel.set_permissions(muteRole, speak=False, send_messages=False)
+
+        await member.add_roles(muteRole, reason=reason)
+        await asyncio.sleep(sleeptime)
+        await member.remove_roles(muteRole, reason="Automatic unmute")
+        embed = discord.Embed(
+            colour=discord.Color.from_rgb(244, 182, 89)
+        )
+        embed.add_field(name="Unmute", value=f"{member} has been automatically unmuted!")
+        await ctx.send(embed=embed)
 
 
 def setup(client):
