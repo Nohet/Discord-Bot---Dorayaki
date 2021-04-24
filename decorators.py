11754aa5cf9f415135ece65bb221cccdcabff4d0
Nohet <70505658+Nohet@users.noreply.github.com>
2021-04-24 11:00:54 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -1,9 +1,15 @@
+import discord
 from database import *
 
 
-def is_registered(ctx):
+async def is_registered(ctx):
     findacc = collection.find_one({"_id": ctx.author.id})
     if not findacc:
         usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False}
         collection.insert_one(usereconomy)
+        embed = discord.Embed(
+            colour=discord.Color.from_rgb(244, 182, 89)
+        )
+        embed.add_field(name="Success", value="Successfully created account!")
+        await ctx.send(embed=embed)
     return True
