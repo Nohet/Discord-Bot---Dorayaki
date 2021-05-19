e89f0802441179c8ff6ada43943207403335db67
Nohet <70505658+Nohet@users.noreply.github.com>
2021-05-19 08:45:17 +0200

    Update database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from config import mongo_token
 
 cluster = MongoClient(mongo_token)
 db = cluster["Discord"]
-collection = db["usersandguilds"]
-guildsett = db["guildssettings"]
-warnsdata = db["warnsdata"]
+
+collection = db["economy"]
+guildsett = db["settings"]
+warnsdata = db["warns"]
