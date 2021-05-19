f20cf03a2d02e00144abcb3cd1049a3ba5f41764
Nohet <70505658+Nohet@users.noreply.github.com>
2021-05-19 08:47:57 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -5,7 +5,7 @@ from database import *
 async def is_registered(ctx):
     findacc = collection.find_one({"_id": ctx.author.id})
     if not findacc:
-        usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False}
+        usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False, "slots": 0, "coinflips": 0, "rob": 0}
         collection.insert_one(usereconomy)
         embed = discord.Embed(
             colour=discord.Color.from_rgb(244, 182, 89)
@@ -19,3 +19,20 @@ async def get_prefix(client, message):
     results = guildsett.find_one({"_id": message.guild.id})
     prefix = results["prefix"]
     return prefix
+
+
+async def convert(time):
+    pos = ["s", "m", "h", "d"]
+
+    time_dict = {"s": 1, "m": 60, "h": 3600, "d": 3600 * 24}
+
+    unit = time[-1]
+
+    if unit not in pos:
+        return -1
+    try:
+        val = int(time[:-1])
+    except:
+        return -2
+
+    return val * time_dict[unit]
