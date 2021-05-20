a4bd32053ad042ded32060fb355ca7e0cc3f382d
Nohet <70505658+Nohet@users.noreply.github.com>
2021-05-20 18:11:08 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,46 +3,45 @@ mongo_token = ""
 owner_id = 000000000000000000
 help1 = "balance, withdraw, deposit, daily, send, coinflip, slots, rob"
 help2 = "ban, mute, tempmute, tempban, warn, kick, slowmode, clear, checkwarns, settings, current_settings"
-help3 = "avatar, say, create_embed, info, userinfo, webhook"
-help4 = "ascii, meme, dog_fact, cat_fact, blush, dance, smile, sleepy, thinking, hello, thumbsup, happy, cry, pikachu, pokedex, joke, mcsrv, achievement, captcha"
+help3 = "avatar, say, create_embed, info, userinfo, webhook, translate, logs"
+help4 = "ascii, meme, dog_fact, cat_fact, koala_fact, panda_fact, fox_fact, hug, wink, pat, bird_fact, blush, dance, " \
+        "smile, sleepy, thinking, hello, thumbsup, happy, cry, pikachu, pokedex, joke, mcsrv, achievement, captcha"
 
 crygifdata = ["https://media.giphy.com/media/10tIjpzIu8fe0/giphy.gif",
-                  "https://media.giphy.com/media/TydZAW0DVCbGE/giphy.gif",
-                  "https://media.giphy.com/media/L95W4wv8nnb9K/giphy.gif",
-                  "https://media.giphy.com/media/qQdL532ZANbjy/giphy.gif"]
+              "https://media.giphy.com/media/TydZAW0DVCbGE/giphy.gif",
+              "https://media.giphy.com/media/L95W4wv8nnb9K/giphy.gif",
+              "https://media.giphy.com/media/qQdL532ZANbjy/giphy.gif"]
 blushgifdata = ["https://media.giphy.com/media/5CnOzYQRSFnWw/giphy.gif",
-                    "https://media.giphy.com/media/PR3wumHIdsBhu/giphy.gif",
-                    "https://media.giphy.com/media/3IQsGPmOK8s4U/giphy.gif",
-                    "https://media.giphy.com/media/hgexLu61GFLr2/giphy.gif"]
+                "https://media.giphy.com/media/PR3wumHIdsBhu/giphy.gif",
+                "https://media.giphy.com/media/3IQsGPmOK8s4U/giphy.gif",
+                "https://media.giphy.com/media/hgexLu61GFLr2/giphy.gif"]
 happygifdata = ["https://media.giphy.com/media/xT5LMHxhOfscxPfIfm/giphy.gif",
-                    "https://media.giphy.com/media/LZElUsjl1Bu6c/giphy.gif",
-                    "https://media.giphy.com/media/14udF3WUwwGMaA/giphy.gif",
-                    "https://media.giphy.com/media/Oi6tJtKNThC6I/giphy.gif"]
+                "https://media.giphy.com/media/LZElUsjl1Bu6c/giphy.gif",
+                "https://media.giphy.com/media/14udF3WUwwGMaA/giphy.gif",
+                "https://media.giphy.com/media/Oi6tJtKNThC6I/giphy.gif"]
 sleepygifdata = ["https://media.giphy.com/media/OjmrBW4ZQbWjkq6RkC/giphy.gif",
-                     "https://media.giphy.com/media/l0MYu38R0PPhIXe36/giphy.gif",
-                     "https://media.giphy.com/media/llZ2R6hwJVxWE/giphy.gif",
-                     "https://media.giphy.com/media/iTOS89Y0gD1ny/giphy.gif"]
+                 "https://media.giphy.com/media/l0MYu38R0PPhIXe36/giphy.gif",
+                 "https://media.giphy.com/media/llZ2R6hwJVxWE/giphy.gif",
+                 "https://media.giphy.com/media/iTOS89Y0gD1ny/giphy.gif"]
 smilegifdata = ["https://media.giphy.com/media/QLvRBqfLXCphu/giphy.gif",
-                    "https://media.giphy.com/media/10t57cXgo7x5kI/giphy.gif",
-                    "https://media.giphy.com/media/vOeX7D6oYKUs8/giphy.gif",
-                    "https://media.giphy.com/media/muHlnG1WU9YfS/giphy.gif"]
+                "https://media.giphy.com/media/10t57cXgo7x5kI/giphy.gif",
+                "https://media.giphy.com/media/vOeX7D6oYKUs8/giphy.gif",
+                "https://media.giphy.com/media/muHlnG1WU9YfS/giphy.gif"]
 thinkgifdata = ["https://media.giphy.com/media/lKXEBR8m1jWso/giphy.gif",
-                    "https://media.giphy.com/media/kPtv3UIPrv36cjxqLs/giphy.gif",
-                    "https://media.giphy.com/media/PoImGf3Takz1BtpNSG/giphy.gif",
-                    "https://media.giphy.com/media/l0HlUsr30YgIHASl2/giphy.gif"]
+                "https://media.giphy.com/media/kPtv3UIPrv36cjxqLs/giphy.gif",
+                "https://media.giphy.com/media/PoImGf3Takz1BtpNSG/giphy.gif",
+                "https://media.giphy.com/media/l0HlUsr30YgIHASl2/giphy.gif"]
 hellogifdata = ["https://media.giphy.com/media/28GHfhGFWpFgsQB4wR/giphy.gif",
-                    "https://media.giphy.com/media/QYkX9IMHthYn0Y3pcG/giphy.gif",
-                    "https://media.giphy.com/media/YrZECW1GgBkqat6F0B/giphy.gif",
-                    "https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif"]
+                "https://media.giphy.com/media/QYkX9IMHthYn0Y3pcG/giphy.gif",
+                "https://media.giphy.com/media/YrZECW1GgBkqat6F0B/giphy.gif",
+                "https://media.giphy.com/media/ASd0Ukj0y3qMM/giphy.gif"]
 thumbsupgifdata = ["https://media.giphy.com/media/3o7absbD7PbTFQa0c8/giphy.gif",
-                       "https://media.giphy.com/media/30pjt9I0SDs7R4ghlp/giphy.gif",
-                       "https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif",
-                       "https://media.giphy.com/media/CvZuv5m5cKl8c/giphy.gif"]
+                   "https://media.giphy.com/media/30pjt9I0SDs7R4ghlp/giphy.gif",
+                   "https://media.giphy.com/media/XreQmk7ETCak0/giphy.gif",
+                   "https://media.giphy.com/media/CvZuv5m5cKl8c/giphy.gif"]
 dancegifdata = ["https://media.giphy.com/media/6fScAIQR0P0xW/giphy.gif",
-                    "https://media.giphy.com/media/3o85xK9ReqLLQD8GL6/giphy.gif",
-                    "https://media.giphy.com/media/l3V0lsGtTMSB5YNgc/giphy.gif",
-                    "https://media.giphy.com/media/pHYaWbspekVsTKRFQT/giphy.gif"]
+                "https://media.giphy.com/media/3o85xK9ReqLLQD8GL6/giphy.gif",
+                "https://media.giphy.com/media/l3V0lsGtTMSB5YNgc/giphy.gif",
+                "https://media.giphy.com/media/pHYaWbspekVsTKRFQT/giphy.gif"]
 
 slotsrandomdata = [":tada:", ":cookie:", ":large_blue_diamond:", ":money_with_wings:", ":moneybag:", ":cherries:"]
-
-
