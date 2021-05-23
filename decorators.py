6e696fc800cfffdef21e12eb090e4309677f41ec
Nohet <70505658+Nohet@users.noreply.github.com>
2021-05-23 09:10:30 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -5,7 +5,8 @@ from database import *
 async def is_registered(ctx):
     findacc = collection.find_one({"_id": ctx.author.id})
     if not findacc:
-        usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False, "slots": 0, "coinflips": 0, "rob": 0}
+        usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False, "slots": 0,
+                       "coinflips": 0, "rob": 0}
         collection.insert_one(usereconomy)
         embed = discord.Embed(
             colour=discord.Color.from_rgb(244, 182, 89)
@@ -36,3 +37,12 @@ async def convert(time):
         return -2
 
     return val * time_dict[unit]
+
+
+async def is_disabled(ctx):
+    embed = discord.Embed(
+        colour=discord.Color.from_rgb(244, 182, 89)
+    )
+    embed.add_field(name="Error", value="This command is disabled!")
+    await ctx.send(embed=embed)
+    return False
