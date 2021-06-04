44aa747142d3b06c95d7aa4d0caa601d98c79442
Nohet <70505658+Nohet@users.noreply.github.com>
2021-06-04 21:37:10 +0200

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -6,7 +6,7 @@ async def is_registered(ctx):
     findacc = collection.find_one({"_id": ctx.author.id})
     if not findacc:
         usereconomy = {"_id": ctx.message.author.id, "wallet": 0, "bank": 0, "received": False, "slots": 0,
-                       "coinflips": 0, "rob": 0}
+                       "coinflips": 0, "rob": 0, "horse_racing": 0}
         collection.insert_one(usereconomy)
         embed = discord.Embed(
             colour=discord.Color.from_rgb(244, 182, 89)
@@ -46,3 +46,18 @@ async def is_disabled(ctx):
     embed.add_field(name="Error", value="This command is disabled!")
     await ctx.send(embed=embed)
     return False
+
+
+async def is_nsfw_enabled(ctx):
+    r = guildsett.find_one({"_id": ctx.guild.id})
+    nsfw = r["nsfw"]
+    if nsfw == "disable" or nsfw == "off" or nsfw == "False":
+        embed = discord.Embed(
+            colour=discord.Color.from_rgb(244, 182, 89)
+        )
+        embed.add_field(name="Error", value="This command is disabled by server owner!")
+        embed.set_footer(text="Type >settings nsfw <on/enable/True> to enable nsfw.")
+        await ctx.send(embed=embed)
+        return False
+    elif nsfw == "enable" or nsfw == "on" or nsfw == "True":
+        return True
